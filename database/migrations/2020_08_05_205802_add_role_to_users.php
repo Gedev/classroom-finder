98c29aa028599c91cb17dc28e21f8bf94ce60c4b
Gedev <gerald.devreux.pro@gmail.com>
2020-08-08 14:54:49 +0200

    add nullable option for users role

--- database/migrations/2020_08_05_205802_add_role_to_users.php
+++ database/migrations/2020_08_05_205802_add_role_to_users.php
@@ -14,7 +14,7 @@ class AddRoleToUsers extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('role');
+            $table->string('role')->nullable;
         });
     }
 
