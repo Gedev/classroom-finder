2467781fee89af49c0d4ec6ce9feddb72e282671
Gedev <gerald.devreux.pro@gmail.com>
2020-08-16 18:45:46 +0200

    Attendance view : css synchronized with the index

--- resources/views/attendanceRecord.blade.php
+++ resources/views/attendanceRecord.blade.php
@@ -4,44 +4,54 @@
 @endsection
 
 @section('realTimeFormValidation')
-<div class="col-md-4 bodyContent">
-    <form>
-        <div class="form-group">
-            <label for="inputGroupSelect01">Select your class</label>
-            <select class="custom-select" id="inputGroupSelect01">
-                <option selected>...</option>
-                @foreach ($classrooms as $classroom)
-                    <option value="{{ $classroom->id }}">
-                        {{ $classroom->id }}
-                    </option>
-                @endforeach
-            </select>
-        </div>
-    </form>
-    <table class="table">
-        <thead>
-        <tr>
-            <th scope="col">Id</th>
-            <th scope="col">Name</th>
-            <th scope="col">Email</th>
-        </tr>
-        </thead>
-        @foreach ($users as $user)
-            <tr class="{{ $user->name }}" id="attendanceRow">
-                <td>{{ $user->id }}</td>
-                <td>{{ $user->name }}</td>
-                <td>{{ $user->email }}</td>
-                <td>{{ $user->idCard }}</td>
-            </tr>
-        @endforeach
-    </table>
+    <div class="container">
+        <div class="bodyContent row justify-content-center">
+            <div class="col-md-4">
+                <p>
+                    <div class="alert-light">
+                        <div class="first-letter-title">A</div>
+                        <div class="title">ttendance</div>
+                    </div>
+                </p>
+                <form>
+                    <div class="form-group">
+                        <label for="inputGroupSelect01">Select your class</label>
+                        <select class="custom-select" id="inputGroupSelect01">
+                            <option selected>...</option>
+                            @foreach ($classrooms as $classroom)
+                                <option value="{{ $classroom->id }}">
+                                    {{ $classroom->id }}
+                                </option>
+                            @endforeach
+                        </select>
+                    </div>
+                </form>
+                <table class="table">
+                    <thead>
+                    <tr>
+                        <th scope="col">Id</th>
+                        <th scope="col">Name</th>
+                        <th scope="col">Email</th>
+                    </tr>
+                    </thead>
+                    @foreach ($users as $user)
+                        <tr class="{{ $user->name }}" id="attendanceRow">
+                            <td>{{ $user->id }}</td>
+                            <td>{{ $user->name }}</td>
+                            <td>{{ $user->email }}</td>
+                            <td>{{ $user->idCard }}</td>
+                        </tr>
+                    @endforeach
+                </table>
 
-    <div>
-        <label for="message">Please use your id card with the card to register your presence.</label>
-        <input class="form-control" placeholder="Enter some text" id="message" type="password" autocomplete="off" />
-        <p id="result"></p>
+                <div>
+                    <label for="message">Please use your id card with the card to register your presence.</label>
+                    <input class="form-control" placeholder="Enter some text" id="message" type="password" autocomplete="off" />
+                    <p id="result"></p>
+                </div>
+            </div>
+        </div>
     </div>
-</div>
 
 @endsection
 
