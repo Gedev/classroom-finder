ddf6726979370a260cef5412876f007467bdb104
Gérald Devreux <gerald.devreux.pro@gmail.com>
2020-08-18 23:56:31 +0200

    Add student view : a few css

--- resources/views/createStudent.blade.php
+++ resources/views/createStudent.blade.php
@@ -1,30 +1,36 @@
 @extends('layouts.app')
 
 @section('content')
-<form action="/addStudent" method="post" class="bodyContent">
+<container>
+    <div class=" row justify-content-center">
+        <div class="col-md-8 bodyContent">
+            <form action="/addStudent" method="post">
 
-    <div class="form-group">
-        <label for="name">Name</label>
-        <input type="text" class="form-control" id="name" placeholder="Enter a name"/>
+                <div class="form-group">
+                    <label for="name">Name</label>
+                    <input type="text" class="form-control" id="name" placeholder="Enter a name"/>
+                </div>
+
+                <div class="form-group">
+                    <label for="exampleInputEmail1">Email address</label>
+                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+
+                </div>
+
+                <div class="form-group">
+                    <label for="idCard">Id card</label>
+                    <input type="text" class="form-control" id="idCard" placeholder="">
+                    <small id="emailHelp" class="form-text text-muted">Use a card unused with the card reader. This field may be completed later.</small>
+                </div>
+
+                <div class="form-group">
+                    <label for="password">Choose a password</label>
+                    <input type="password" id="password" class="form-control"/>
+                </div>
+
+                <button type="submit" class="btn btn-primary" value="addStudent">Submit</button>
+            </form>
+        </div>
     </div>
-
-    <div class="form-group">
-        <label for="exampleInputEmail1">Email address</label>
-        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-
-    </div>
-
-    <div class="form-group">
-        <label for="idCard">Id card</label>
-        <input type="text" class="form-control" id="idCard" placeholder="">
-        <small id="emailHelp" class="form-text text-muted">Use a card unused with the card reader. This field may be completed later.</small>
-    </div>
-
-    <div class="form-group">
-        <label for="password">Choose a password</label>
-        <input type="password" id="password" class="form-control"/>
-    </div>
-
-    <button type="submit" class="btn btn-primary" value="addStudent">Submit</button>
-</form>
+</container>
 @endsection
