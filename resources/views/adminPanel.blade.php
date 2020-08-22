8be62f54fdbf7a065273fe6abf7c6fecee278103
Gérald Devreux <gerald.devreux.pro@gmail.com>
2020-08-22 22:40:40 +0200

    Some CSS : Delete bootstrap cards

--- resources/views/adminPanel.blade.php
+++ resources/views/adminPanel.blade.php
@@ -6,10 +6,9 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
+    <div class="bodyContent row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Overview') }}</div>
                 <div class="card-body">
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
--- resources/views/classrooms/index.blade.php
+++ resources/views/classrooms/index.blade.php
@@ -12,43 +12,37 @@
         </ol>
     </nav>
     <div class="container">
-        <div class="row justify-content-center">
+        <div class="bodyContent row justify-content-center">
             <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">{{ __('Dashboard') }}</div>
-
-                    <div class="card-body">
-                        @if (session('status'))
-                            <div class="alert alert-success" role="alert">
-                                {{ session('status') }}
-                            </div>
-                        @endif
-                        <h4>Classrooms</h4>
-                            @if(Auth::user()->role == 'director')
-                                <a href="{{ route('classrooms.create') }}" class="btn btn-success">(+) Add a student</a>
-                            @endif
-                        <table class="table">
-                            <thead>
-                                <tr>
-                                    <th scope="col">Id</th>
-                                    <th scope="col">Floor</th>
-                                    <th scope="col">Number of seats</th>
-                                    <th scope="col">Has whiteboard</th>
-                                    <th scope="col">Has projector</th>
-                                </tr>
-                            </thead>
-                            @foreach ($classrooms as $classroom)
-                                <tr>
-                                    <td>{{ $classroom->id }}</td>
-                                    <td>{{ $classroom->floor }}</td>
-                                    <td>{{ $classroom->nb_of_seats }}</td>
-                                    <td>{{ $classroom->has_whiteboard }}</td>
-                                    <td>{{ $classroom->has_projector }}</td>
-                                </tr>
-                            @endforeach
-                        </table>
+                @if (session('status'))
+                    <div class="alert alert-success" role="alert">
+                        {{ session('status') }}
                     </div>
-                </div>
+                @endif
+                <h4>Classrooms</h4>
+                    @if(Auth::user()->role == 'director')
+                        <a href="{{ route('classrooms.create') }}" class="btn btn-success">(+) Add a student</a>
+                    @endif
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">Id</th>
+                            <th scope="col">Floor</th>
+                            <th scope="col">Number of seats</th>
+                            <th scope="col">Has whiteboard</th>
+                            <th scope="col">Has projector</th>
+                        </tr>
+                    </thead>
+                    @foreach ($classrooms as $classroom)
+                        <tr>
+                            <td>{{ $classroom->id }}</td>
+                            <td>{{ $classroom->floor }}</td>
+                            <td>{{ $classroom->nb_of_seats }}</td>
+                            <td>{{ $classroom->has_whiteboard }}</td>
+                            <td>{{ $classroom->has_projector }}</td>
+                        </tr>
+                    @endforeach
+                </table>
             </div>
         </div>
     </div>
