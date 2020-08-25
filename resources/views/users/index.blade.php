99469d64ae3c4d7903de3e9a648a78d9605a6d7f
Gérald Devreux <gerald.devreux.pro@gmail.com>
2020-08-25 23:35:29 +0200

    User list : add filters buttons (WIP)

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -14,83 +14,92 @@
 </nav>
 <div class="container">
     <div class="bodyContent row justify-content-center">
-        @if (session()->has('success'))
-            <div class="alert alert-success" role="alert">
-                {{ session()->get('success') }}
-            </div>
-        @endif
+        <div class="col-md-8">
+            @if (session()->has('success'))
+                <div class="alert alert-success" role="alert">
+                    {{ session()->get('success') }}
+                </div>
+            @endif
 
-        <h4>Users</h4>
-        <table class="table">
-            <thead>
-            <tr>
-                <th scope="col">Id</th>
-                <th scope="col">Name</th>
-                <th scope="col">Email</th>
-                <th scope="col">Role</th>
-            </tr>
-            </thead>
+            <h4>Users</h4>
+            @if(Auth::user()->role == 'director')
+                Add filter :
+                <a href="" class="btn btn-info">Id</a>
+                <a href="" class="btn btn-info">Name</a>
+                <a href="" class="btn btn-info">Email</a>
+                <a href="" class="btn btn-info">Role</a>
+            @endif
+            <table class="table">
+                <thead>
+                <tr>
+                    <th scope="col">Id</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">Email</th>
+                    <th scope="col">Role</th>
+                </tr>
+                </thead>
 
-            @php $toEnd = count($users); @endphp
-            @foreach ($users as $user)
-                @if (isset($_COOKIE["newUserData"]))
-                    @if(0 === --$toEnd && $_COOKIE["newUserData"]==1)
-                        <tr class="justCreated">
-                    @else
-                        <tr>
-                    @endif
-                @endif
-                        <td>{{ $user->id }}</td>
-                        <td>{{ $user->name }}</td>
-                        <td>{{ $user->email }}</td>
-                        @if($user->role)
-                            <td>{{ $user->role }}</td>
+                @php $toEnd = count($users); @endphp
+                @foreach ($users as $user)
+                    @if (isset($_COOKIE["newUserData"]))
+                        @if(0 === --$toEnd && $_COOKIE["newUserData"]==1)
+                            <tr class="justCreated">
                         @else
-                            <td><span class="text-danger" id="basic-addon3">
-                                <i>NO ROLE ASSIGNED</i>
-                            </span></td>
+                            <tr>
                         @endif
-                        <td>
-                            <a href="{{ route('users.edit',$user->id)}}" class="btn btn-primary">Edit</a>
-                        </td>
-                        <td>
-                            <form action="{{ route('users.destroy', $user->id)}}" method="post" id="deleteUserForm">
-                                @csrf
-                                @method('DELETE')
-                                {{-- DELETE BUTTON --}}
-                                <button type="button" class="btn btn-danger" onclick="deleteData({{$user->id}})" data-toggle="modal" data-target="#confirmDeleteModal<?= $user->id?>">Delete</button>
+                    @endif
+                            <td>{{ $user->id }}</td>
+                            <td>{{ $user->name }}</td>
+                            <td>{{ $user->email }}</td>
+                            @if($user->role)
+                                <td>{{ $user->role }}</td>
+                            @else
+                                <td><span class="text-danger" id="basic-addon3">
+                                    <i>NO ROLE ASSIGNED</i>
+                                </span></td>
+                            @endif
+                            <td>
+                                <a href="{{ route('users.edit',$user->id)}}" class="btn btn-primary">Edit</a>
+                            </td>
+                            <td>
+                                <form action="{{ route('users.destroy', $user->id)}}" method="post" id="deleteUserForm">
+                                    @csrf
+                                    @method('DELETE')
+                                    {{-- DELETE BUTTON --}}
+                                    <button type="button" class="btn btn-danger" onclick="deleteData({{$user->id}})" data-toggle="modal" data-target="#confirmDeleteModal<?= $user->id?>">Delete</button>
 
-                                <!-- MODAL -->
-                                <div class="modal fade" id="confirmDeleteModal<?= $user->id?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                                    <div class="modal-dialog" role="document">
-                                        <div class="modal-content">
-                                            <div class="modal-header">
-                                                <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-                                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                                    <span aria-hidden="true">&times;</span>
-                                                </button>
-                                            </div>
-                                            <div class="modal-body">
-                                                Do you really want to delete this record? This process cannot be undone.
-                                                {{ $user->id }} / {{ $user->name }}
-                                            </div>
-                                            <div class="modal-footer">
-                                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                                                <button class="btn btn-danger" type="submit" onclick="confirmDeleteUser()">Confirm Delete</button>
+                                    <!-- MODAL -->
+                                    <div class="modal fade" id="confirmDeleteModal<?= $user->id?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                        <div class="modal-dialog" role="document">
+                                            <div class="modal-content">
+                                                <div class="modal-header">
+                                                    <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                        <span aria-hidden="true">&times;</span>
+                                                    </button>
+                                                </div>
+                                                <div class="modal-body">
+                                                    Do you really want to delete this record? This process cannot be undone.
+                                                    {{ $user->id }} / {{ $user->name }}
+                                                </div>
+                                                <div class="modal-footer">
+                                                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                                    <button class="btn btn-danger" type="submit" onclick="confirmDeleteUser()">Confirm Delete</button>
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
-                                </div>
-                                {{-- END MODAL --}}
-                            </form>
-                        </td>
-                    </tr>
-            @endforeach
-        </table>
-            @if(Auth::user()->role == 'director')
-                <a href="{{ route('users.create') }}" class="btn btn-success">(+) Add a user</a>
-            @endif
-            {{ $users->links() }}
-        <button class="btn btn-danger" type="submit" onclick="ReadCookie()">Check cookie</button>
+                                    {{-- END MODAL --}}
+                                </form>
+                            </td>
+                        </tr>
+                @endforeach
+            </table>
+                @if(Auth::user()->role == 'director')
+                    <a href="{{ route('users.create') }}" class="btn btn-success">(+) Add a user</a>
+                @endif
+                {{ $users->links() }}
+            <button class="btn btn-danger" type="submit" onclick="ReadCookie()">Check cookie</button>
+        </div>
 </div>
 @endsection
