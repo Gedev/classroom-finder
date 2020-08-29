c7ecfc80415b139a93a9372f482009fffe22c01f
Gérald Devreux <gerald.devreux.pro@gmail.com>
2020-08-29 22:59:16 +0200

    Rearrange CSS of the adminPanel view

--- resources/views/adminPanel.blade.php
+++ resources/views/adminPanel.blade.php
@@ -6,67 +6,80 @@
 
 @section('content')
 <div class="container">
-    <div class="bodyContent row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-                    <h4>Permissions</h4>
-                    <a class="btn btn-primary" href="{{ Route('permissions') }}">Permissions</a>
+    <div class="bodyContent justify-content-center">
+        <div class="alert-light">
+            <div class="first-letter-title">A</div>
+            <div class="title">dmin Panel</div>
+        </div>
+        
+        <div class="row">
+            <div class="col-sm-6">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Permissions</h5>
+                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                        <a class="btn btn-primary" href="{{ Route('permissions') }}">Permissions</a>
+                    </div>
                 </div>
             </div>
-            <div class="card">
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-                    <h4>Students</h4>
+            <div class="col-sm-6">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Students</h5>
+                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
                         <a class="btn btn-primary" href="{{ Route('users.index') }}">List of users</a>
-                </div>
-            </div>
-
-            <div class="card">
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-                    <h4>Classrooms</h4>
-                    <a class="btn btn-primary" href="{{ Route('classrooms.index') }}">List of Classrooms</a>
-                </div>
-            </div>
-
-            <div class="card">
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-                    <h4>Courses</h4>
-                    <a class="btn btn-primary" href="{{ Route('courses.index') }}">List of courses</a>
-                </div>
-            </div>
-
-            <div class="card">
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-                    <h4>Sections</h4>
-                    <a class="btn btn-primary" href="{{ Route('sections.index') }}">List of sections</a>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-</div>
+
+        <div class="row">
+            <div class="col-sm-6">
+                <div class="card">
+                    <div class="card-body">
+                        @if (session('status'))
+                            <div class="alert alert-success" role="alert">
+                                {{ session('status') }}
+                            </div>
+                        @endif
+                            <h5 class="card-title">Classrooms</h5>
+                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                        <a class="btn btn-primary" href="{{ Route('classrooms.index') }}">List of Classrooms</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6">
+                <div class="card">
+                    <div class="card-body">
+                        @if (session('status'))
+                            <div class="alert alert-success" role="alert">
+                                {{ session('status') }}
+                            </div>
+                        @endif
+                        <h5 class="card-title">Courses</h5>
+                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                        <a class="btn btn-primary" href="{{ Route('courses.index') }}">List of courses</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-6">
+                <div class="card">
+                    <div class="card-body">
+                        @if (session('status'))
+                            <div class="alert alert-success" role="alert">
+                                {{ session('status') }}
+                            </div>
+                        @endif
+                            <h5 class="card-title">Sections</h5>
+                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                        <a class="btn btn-primary" href="{{ Route('sections.index') }}">List of sections</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div> {{-- END CONTAINER --}}
+</div> {{-- END BODYCONTENT --}}
 @endsection
